479e63ba7b62b6758af9e4dceca62ffeaef4178a
Cristian Macena <cristian@reacaonet.com.br>
2022-01-17 20:36:31 -0300

    Pagination

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -30,12 +30,14 @@ function Listing() {
             });
     }, [pageNumber]);
 
-
+    const handlePageChange = (newNumber: number) => {
+        setPageNumber(newNumber);
+    }
 
     return (
 
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
 
